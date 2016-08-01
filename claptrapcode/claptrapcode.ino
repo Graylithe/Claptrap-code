aea223549ec057c8147b4c42707c2b38a9964b4b
graylithe <grplicu@yahoo.com>
2016-08-01 18:21:28 +0800

    working
    working motor running based on angle

--- claptrapcode/claptrapcode.ino
+++ claptrapcode/claptrapcode.ino
@@ -1,13 +1,311 @@
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+/*
+
+PROJECT CLAPTRAP ARDUINO CODE
+
+Author: Gil Rupert Licu
+Last update: July 2016
+
+Current goal: robot balancing
+
+*/
+
+
+// ================================================================
+// ===              LIBRARIES AND VARIABLE DEFINES              ===
+// ================================================================
+
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+	#include "Wire.h"
+#endif
+
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+#define PWM_A 5
+#define PWM_B 6
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+int16_t gyro[3];        // [x, y, z]            gyro vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+float out;
+float angle;
+float anglespeed;
+float angleerror;
+float expectedangle;
+float initialangle;
+float anglelimithigh;
+float anglelimitlow;
+float lowoutthreshold;
+float outprev;
+float kp;
+float ki;
+float kd;
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
 }
 
-// the loop function runs over and over again forever
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (115200 chosen because it is required for Teapot Demo output, but it's
+    // really up to you depending on your project)
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
+    // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
+    // the baud timing being too misaligned with processor ticks. You must use
+    // 38400 or slower in these cases, or use some kind of external separate
+    // crystal solution for the UART timer.
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready
+    //Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    //while (Serial.available() && Serial.read()); // empty buffer
+    //while (!Serial.available());                 // wait for data
+    //while (Serial.available() && Serial.read()); // empty buffer again
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(PWM_A, OUTPUT);
+    pinMode(PWM_B, OUTPUT);
+
+    digitalWrite(PWM_A, LOW);
+    digitalWrite(PWM_B, LOW);
+
+    initialangle = 40.00;
+    expectedangle = initialangle;
+    anglelimithigh = 20;
+    anglelimitlow = -20;
+    lowoutthreshold = 40;
+	outprev = 0;
+
+    delay(5000);
+}
+
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        // other program behavior stuff here
+        // .
+        // .
+        // .
+        // if you are really paranoid you can frequently test in between other
+        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
+        // while() loop to immediately process the MPU data
+        // .
+        // .
+        // .
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        mpu.dmpGetGyro(gyro, fifoBuffer);
+
+        
+        /*
+        Serial.print("gyro\t");
+        Serial.print((double)gyro[0]);
+        Serial.print("\t");
+        Serial.print((double)gyro[1]);
+        Serial.print("\t");
+        Serial.print((double)gyro[2]);
+        */
+          
+        /*
+        Serial.print("ypr\t");
+        Serial.print(ypr[0] * 180/M_PI);
+        Serial.print("\t");
+        Serial.print(ypr[1] * 180/M_PI);
+        Serial.print("\t");
+        Serial.print(ypr[2] * 180/M_PI);
+         */
+        
+        
+        angle = ypr[1]*180/M_PI - initialangle;
+        anglespeed = (float)gyro[1];
+        
+        //angleerror = angle - expectedangle;
+        
+        //out = ( pow(( (ypr[1]-0.02)*60 ), 3)*5000 ) - ( pow( ((gyro[2]+4)*0.4), 5)*60 );
+        //out = ( ((ypr[1]*180/M_PI)-39)*40 ) - ( gyro[2]*10 );
+		//kp = pow(angle*1,3)*20;
+		kp = angle*60;
+		ki = -pow(anglespeed*0.1,3)*50;
+		//kd = pow(angleerror*1.2,3)*40;
+        //out = kp + ki + kd;
+		out = kp;
+		//out = (outprev + out)/2;
+        /*if (out>0){
+          if(out<lowoutthreshold){
+            out = 0;
+          }
+        }else{
+          if(out>(-lowoutthreshold)){
+            out = 0;
+          }
+        }*/
+		//if (angle<anglelimitlow || angle>anglelimithigh){
+        //  out = 0;
+        //}
+        if (angle<anglelimitlow || angle>anglelimithigh || (out>0 && out<lowoutthreshold) || (out<0 && out>(-lowoutthreshold))){
+          out = 0;
+        }
+        
+        //expectedangle = angle*0.5;
+		//expectedangle = angle/(abs(out));
+		//outprev = out;
+        
+        Serial.print("out=");
+        Serial.print(out);
+		Serial.print(" kp=");
+        Serial.print(kp);
+		Serial.print(" ki=");
+        Serial.print(ki);
+		Serial.print(" kd=");
+        Serial.println(kd);
+        //Serial.print(" angle=");
+        //Serial.print(angle);
+        //Serial.print(" anglespeed=");
+        //Serial.print(anglespeed);
+        //Serial.print(" angleerror=");
+        //Serial.println(angleerror);
+
+        //out=0;
+        if (out>0){
+          digitalWrite(PWM_A, LOW);
+          analogWrite(PWM_B, constrain(out,0,255));
+        }else{
+          digitalWrite(PWM_B, LOW);
+          analogWrite(PWM_A, constrain(-out,0,255));
+        }
+        
+
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
 }
